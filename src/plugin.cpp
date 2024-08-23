56fb18ac68e5f873f8ad0f3625d835ccfd1270ae
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-08-23 13:14:57 +0100

    add

---
+++ src/plugin.cpp
@@ -0,0 +1,34 @@
+#include "log.h"
+#include "hook.h"
+
+void OnDataLoaded()
+{
+   
+}
+
+void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
+{
+	switch (a_msg->type) {
+	case SKSE::MessagingInterface::kDataLoaded:
+		hooks::on_animation_event::install();
+
+		break;
+		
+	default:
+		break;
+	}
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+    SKSE::Init(skse);
+	SetupLog();
+
+
+    auto messaging = SKSE::GetMessagingInterface();
+	if (!messaging->RegisterListener("SKSE", MessageHandler)) {
+		return false;
+	}
+
+	
+    return true;
+}
